e860ce9ca3ba609c69b378f94954f74e3d589dc8
mamluatul khasanah <mamluatulkhasanahsaja@gmail.com>
2026-05-03 18:22:48 +0700

    praktikum 2 pw3,retrieving single models

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -2,22 +2,20 @@
 
 namespace App\Http\Controllers;
 
-use App\Models\UserModel;
 use Illuminate\Http\Request;
-use Illuminate\Support\Facades\Hash; //
+use App\Models\UserModel;
+use Illuminate\Support\Facades\Hash;
 
 class UserController extends Controller
-{ 
-    public function index()
 {
-    // tambah data user dengan Eloquent Model
-    $data = [
-        'nama' => 'Pelanggan Pertama',
-    ];
-    UserModel::where('username', 'customer-1')->update($data); // update data user
+  public function index()
+{
+    // Cari user dengan ID 1, ambil kolom username & nama saja.
+    // Jika tidak ada, jalankan fungsi abort(404).
+    $user = UserModel::findOr(1, ['username', 'nama'], function () {
+        abort(404);
+    });
 
-    // coba akses model UserModel
-    $user = UserModel::all(); // ambil semua data dari tabel m_user
     return view('user', ['data' => $user]);
 }
 }
--- app/Models/UserModel.php
+++ app/Models/UserModel.php
@@ -3,9 +3,19 @@
 namespace App\Models;
 
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Factories\HasFactory;
 
 class UserModel extends Model
 {
+    use HasFactory;
+
     protected $table = 'm_user';
     protected $primaryKey = 'user_id';
+
+    /**
+     * The attributes that are mass assignable.
+     *
+     * @var array
+     */
+    protected $fillable = ['level_id', 'username', 'nama','password'];
 }
